8db0a5679366813063a2016c630f4e3d32440f94
OFF LUCK <ol-sidorenkov@mail.ru>
2021-02-20 12:50:27 +0300

    Update TopSnowfallsSolver.cs

--- Solvers/TopSnowfallsSolver.cs
+++ Solvers/TopSnowfallsSolver.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 
@@ -24,6 +25,28 @@ namespace Proga2Semester1Sem
                     .ToArray();
                 var top = topSnowyTowns[0];
                 Console.WriteLine($"For the year: {group.Key} most snowy is {top.City} with duration (in days): {(int)top.To.Subtract(top.From).TotalDays}, beggining at : {top.From} and ending at: {top.To}");
+            }
+        }
+        
+        
+        public override void Sel()
+        {
+            var townsByYears =
+                from d in Data
+                where d.type == WeatherType.Snow
+                group d by d.startTime.Year;
+            foreach (var g in townsByYears)
+            {
+                var groupedTowns =
+                    from d in g
+                    select new {Year = g.Key, City = d.city, From = d.startTime, To = d.endTime};
+                var sortedTowns =
+                    from d in groupedTowns
+                    orderby d.To.Subtract(d.From) descending
+                    select d;
+                var t = sortedTowns.ToArray();
+                var top = t[0];
+                Console.WriteLine($"For the year: {g.Key} most snowy is {top.City} with duration (in days): {(int)top.To.Subtract(top.From).TotalDays}, beggining at : {top.From} and ending at: {top.To}");
             }
         }
     }
